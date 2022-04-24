5954ab6682333f7d9141e7c1d8f547304f577023
Cj <100457169+cjpriedel@users.noreply.github.com>
2022-04-24 10:26:47 -0700

    completed

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,42 +15,153 @@
 
 // a) Create a test with expect statements using the variables provided.
 
-const secretCodeWord1 = "Lackadaisical"
+// PSEUDOCODE
+// make function that takes string
+// use .replace to check string and replace designated characters 
+    // use gi for global index, reduces case sensitivity errors
+// create a chain of variables, .replace does not change original string
+// return final variable containing original string 
+
+
+describe("l33tSp34k", () => {
+    it("Create a function that takes in a string and returns a coded message", () => {
+    const secretCodeWord1 = "Lackadaisical"
+    const secretCodeWord2 = "Gobbledygook"
+    const secretCodeWord3 = "Eccentric"
+      expect(l33tSp34k(secretCodeWord1)).toEqual("L4ck4d41s1c4l")
+      expect(l33tSp34k(secretCodeWord2)).toEqual("G0bbl3dyg00k")
+      expect(l33tSp34k(secretCodeWord3)).toEqual("3cc3ntr1c")
+    })
+  })
+
+//   FAIL  ./code-challenges.test.js
+//   l33tSp34k
+//     × Create a function that takes in a string and returns a coded message (1 ms)
+
+//   ● l33tSp34k › Create a function that takes in a string and returns a coded message
+
 // Expected output: "L4ck4d41s1c4l"
-const secretCodeWord2 = "Gobbledygook"
 // Expected output: "G0bbl3dyg00k"
-const secretCodeWord3 = "Eccentric"
 // Expected output: "3cc3ntr1c"
 
 
 // b) Create the function that makes the test pass.
 
+const l33tSp34k = (str) => {
+    let stringWithA = str.replace(/a/gi, "4")
+    let stringWithE = stringWithA.replace(/e/gi, "3")
+    let stringWithI = stringWithE.replace(/i/gi, "1")
+    let stringWithO = stringWithI.replace(/o/gi, "0")
+    return stringWithO
+  }
+
+//   PASS  ./code-challenges.test.js
+//   l33tSp34k
+//     √ Create a function that takes in a string and returns a coded message (3 ms)
 
 // --------------------2) Create a function that takes in an array of words and a single letter and returns all the words that contain that particular letter.
 
 // a) Create a test with expects statement using the variable provided.
 
+//PSEUDOCODE
+// use .filter with .includes as parameter that takes a letter variable  
+// add .toUpperCase to cover case sensitivity. 
+
+
+describe("returnItemsOfLetter", () => {
+    it("returns items in array that contain letter input", () => {
+    const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
+    const letterA = "a"
+    const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
+    const letterE = "e"
+        expect(returnItemsOfLetter(arrayOfWords1, letterA)).toEqual(["Apple", "Banana", "Orange"])
+        expect(returnItemsOfLetter(arrayOfWords2, letterE)).toEqual(["Cherry", "Blueberry", "Peach"])
+    })
+  })
+
+//   FAIL  ./code-challenges.test.js
+//   returnItemsOfLetter
+//     × returns items in array that contain letter input
+
+//   ● returnItemsOfLetter › returns items in array that contain letter input
+
+// // Expected output: ["Apple", "Banana", "Orange"]
+// // Expected output: ["Cherry", "Blueberry", "Peach"]
+
+// // b) Create the function that makes the test pass.
+
+const returnItemsOfLetter = (arr, letter) => {
+    return arr.filter(val => {
+      return val.includes(letter) || val.includes(letter.toUpperCase())
+    })
+  }
 const arrayOfWords1 = ["Apple", "Banana", "Plum", "Orange", "Kiwi"]
 const letterA = "a"
-// Expected output: ["Apple", "Banana", "Orange"]
 const arrayOfWords2 = ["Mango", "Cherry", "Apricot", "Blueberry", "Peach"]
 const letterE = "e"
-// Expected output: ["Cherry", "Blueberry", "Peach"]
 
-
-// b) Create the function that makes the test pass.
-
+// PASS  ./code-challenges.test.js
+// returnItemsOfLetter
+//   √ returns items in array that contain letter input (3 ms)
 
 // --------------------3) Create a function that takes in an array of 5 numbers and determines whether or not the array is a “full house”. A full house is exactly one pair and one three of a kind.
 
 // a) Create a test with expect statements using the variable provided.
 
-const hand1 = [5, 5, 5, 3, 3]
+//PSEUDOCODE
+// create function that takes an array
+// if any 3 values match and the remaining two values match each other, return true
+  // anything else is false
+
+describe("fullHouse", () => {
+    it("if any 3 values match and the remaining two values match each other, return true.", () => {
+    const hand1 = [5, 5, 5, 3, 3]
+    const hand2 = [5, 5, 3, 3, 4]
+    const hand3 = [5, 5, 5, 5, 4]
+        expect(fullHouse(hand1)).toEqual(true)
+        expect(fullHouse(hand2)).toEqual(false)
+        expect(fullHouse(hand3)).toEqual(false)
+    })
+  })
+
 // Expected output: true
-const hand2 = [5, 5, 3, 3, 4]
 // Expected output: false
-const hand3 = [5, 5, 5, 5, 4]
 // Expected output: false
 
+// FAIL  ./code-challenges.test.js
+// fullHouse
+//   × if any 3 values match and the remaining two values match each other, return true. (1 ms)
+
+// ● fullHouse › if any 3 values match and the remaining two values match each other, return true.
 
 // b) Create the function that makes the test pass.
+
+const fullHouse = (arr) => {
+    if (arr[0] === arr[1,2] && arr[3] === arr[4] || 
+        arr[0] === arr[2,3] && arr[1] === arr[4] ||
+        arr[0] === arr[3,4] && arr[1] === arr[2])
+        return true
+    else{
+        return false
+    }
+ }
+
+//  PASS  ./code-challenges.test.js
+//   fullHouse
+//     √ if any 3 values match and the remaining two values match each other, return true. (3 ms)
+
+
+// PASS  ./code-challenges.test.js
+// l33tSp34k
+//   √ Create a function that takes in a string and returns a coded message (2 ms)
+// returnItemsOfLetter
+//   √ returns items in array that contain letter input
+// fullHouse
+//   √ if any 3 values match and the remaining two values match each other, return true.
+
+// Test Suites: 1 passed, 1 total
+// Tests:       3 passed, 3 total
+// Snapshots:   0 total
+// Time:        0.476 s, estimated 1 s
+// Ran all test suites.
+// Done in 1.41s.
--- interview-questions.md
+++ interview-questions.md
@@ -7,53 +7,53 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is a hash in Ruby?
 
-  Your answer:
+  Your answer: a collection of key value pairs
 
-  Researched answer:
+  Researched answer: Hashes are key value pair collections, allowing developers to access any object using special "key" values to find. 
 
 
 
 2. What is a gem?
 
-  Your answer:
+  Your answer: A gem is a library consisting of predefined code, documentation and even tests.  
 
-  Researched answer:
+  Researched answer: A gem serves similarly to a plug in for the programming language Ruby. Gem's are collections of codes and documents to improve the quality of life for developers during programming. 
 
 
 
 3. What is Ruby on Rails?
 
-  Your answer:
+  Your answer: A framework built on Ruby, providing developers with an easier language to develop quicker
 
-  Researched answer:
+  Researched answer: Ruby on Rails is a framework built on Ruby. RoR provides straightforward, intuitive framework to improve efficiency. 
 
 
 
 4. What is a relational database? Are there other kinds of databases?
 
-  Your answer:
+  Your answer: Relational databases are stored in data tables.
 
-  Researched answer:
+  Researched answer: Relational databases are similar to the way info can be stored in Microsoft Excel, using columns and rows with labels to identify data by using all tables that are connected to each other. Non-relational databases can perform slower than their counterpart, and anything can be stored as a value, as long as a key or name is assigned to it. 
 
 
 
 5. What are primary keys? Why are they important?
 
-  Your answer:
+  Your answer: The first unique id available for reference when attempting to access data from a relational database, using primary keys avoids issues if there are two data values under a similar name
 
-  Researched answer:
+  Researched answer: Primary keys are used to sort, create and access data in a database by using a unique id value, the primary key. 
 
 
 
 ## Looking Ahead: Terms for Next Week
 Research and define the following terms to the best of your ability.
 
-1. RESTful routes:
+1. RESTful routes: Representational State Transfer. A pattern for paths between the server and user
 
-2. JSON:
+2. JSON: JavaScript Object Notation, used to store/transport data in a simplified way. 
 
-3. ERB:
+3. ERB: Embedded Ruby. refers to the Ruby code in HTML 
 
-4. Params:
+4. Params: The data that is in the app during an HTTP request, running through get and post requests
 
-5. API:
+5. API: Application Programming Interface, transfers data from online/outside the app to be used within the app.
